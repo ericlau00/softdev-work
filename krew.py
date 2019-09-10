046769d4ce91d820784319cde26428a6b40edc60
ericlau00 <elau00@stuy.edu>
2019-09-10 08:45:28 -0400

    in progress

---
+++ krew.py
@@ -0,0 +1,15 @@
+import random 
+
+KREWES = {
+	'orpheus': ['Emily', 'Kevin'],
+	'rex': ['William', 'Joseph'],
+	'endymion': ['Grace', 'Nahi']
+}
+
+def pick(): 
+	random_team = random.randrange(len(KREWES.keys()))
+	random_person = 
+	print(random_person)
+pick()
+
+
