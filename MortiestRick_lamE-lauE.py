0e8274dc2b945e0d5ce3a45838a16bbbb5da7313
ericlau00 <sharkguy418@gmail.com>
2019-09-11 13:42:51 -0400

    merge

---
+++ MortiestRick_lamE-lauE.py
@@ -0,0 +1,15 @@
+import random
+def printRandFromDict(dictionary):
+    #Create a list of keys
+    keys = list(dictionary.keys())
+    #Generate a random team
+    team = dictionary[keys[random.randrange(len(keys))]]
+    #Print a random member fo the generated team
+    print(team[random.randrange(len(team))])
+    
+KREWES = {
+	'orpheus': ['Emily', 'Kevin', 'Vishwaa', 'Eric', 'ray', 'Jesse', 'Tiffany', 'Amanda', 'Junhee', 'Jackie ', 'Tyler', 'Emory', 'Ivan', 'Elizabeth', 'Pratham', 'Shaw', 'Eric', 'Yaru', 'Kelvin', 'Hong Wei', 'Michael', 'Kiran', 'Amanda', 'Joseph', 'Tanzim', 'David', 'Yevgeniy'],
+	'rex': ['William', 'Joseph', 'Calvin', 'Ethan', 'Moody', 'Mo', 'Big Mo', 'Peihua', 'Saad', 'Benjamin', 'Justin', 'Alice', 'Hilary', 'Ayham', 'Michael', 'Matthew', 'Jionghao', 'Devin ', 'David', 'Jacob', 'Will', 'Hannah', 'Alex'],
+	'endymion': ['Grace', 'Nahi', 'Derek', 'Jun Tao', 'Connor', 'Jason', 'Tammy', 'Albert', 'Kazi', 'Derek', 'Brandon', 'Kenneth', 'Lauren', 'Biraj', 'Jeff', 'Jackson', 'Taejoon', 'Kevin', 'Jude', 'Sophie', 'Henry', 'Coby', 'Manfred', 'Leia', 'Ahmed', 'Winston']
+}
+printRandFromDict(KREWES)
