061aa0152d0518bd1b12ca8f6cf74a72f8ed5160
ericlau00 <ericlau41802@gmail.com>
2019-10-07 14:11:33 -0400

    work

--- 17_csv2db/db_builder.py
+++ 17_csv2db/db_builder.py
@@ -7,21 +7,37 @@ import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
 
 DB_FILE="discobandit.db"
-
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
-c = db.cursor()               #facilitate db ops
+c = db.cursor()             #facilitate db ops
 
-#==========================================================
+command = "CREATE TABLE courses(code TEXT, mark INTEGER, id INTEGER);"
+c.execute(command)
 
-# < < < INSERT YOUR POPULATE-THE-DB CODE HERE > > >
+with open('courses.csv') as courses:
+    reader = csv.DictReader(courses)
+    for row in reader:
 
+        code = "\"" + row['code'] + "\""
+        mark = row['mark']
+        id = row['id']
 
-command = "INSERT INTO roster VALUES(\"joseph\", 0)"          # test SQL stmt in sqlite3 shell, save as string
-c.execute(command)    # run SQL statement
+        command = "INSERT INTO courses VALUES(" + code + "," + mark + "," + id + ");"
+        c.execute(command)
 
-#==========================================================
+command = "CREATE TABLE students(name TEXT, age INTEGER, id INTEGER);"
+c.execute(command)
 
-db.commit() #save changes
-db.close()  #close database
+with open('students.csv') as students:
+    reader = csv.DictReader(students)
+    for row in reader:
 
+        name = "\"" + row['name'] + "\""
+        age = row['age']
+        id = row['id']
+
+        command = "INSERT INTO students VALUES(" + name + "," + age + "," + id + ");"
+        c.execute(command)
+
+db.commit()
+db.close()
 
