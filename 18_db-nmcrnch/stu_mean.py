38257ed97cd7ed4ab455240172e9151a2c2eb062
ericlau00 <ericlau41802@gmail.com>
2019-10-14 13:07:54 -0400

    combine w YG

--- 18_db-nmcrnch/stu_mean.py
+++ 18_db-nmcrnch/stu_mean.py
@@ -10,42 +10,33 @@ DB_FILE='discobandit.db'
 db = sqlite3.connect(DB_FILE)
 c = db.cursor()
 
-names = {}
-grades = {}
-averages = {}
-
-data = c.execute(
-    """
+data = c.execute('''
     SELECT students.id, students.name
-    FROM students;
-    """
-)
+    FROM students; ''')
 
-for id, name in data:
-    names[id] = name
-    grades[id] = []
+#the key is the student's id
+#the value is a array which houses the name, number of classes, and sum of grades 
+dictionary = {id: [name, 0, 0] for id, name in data}
 
-data = c.execute(
-    """
+data = c.execute('''
     SELECT students.id, mark
     FROM courses, students
-    WHERE courses.id = students.id;
-    """
-)
+    WHERE courses.id = students.id;''')
 
 for id, mark in data:
-    grades[id].append(mark)
+    grades[id][0] += 1 #number of classes is incremented
+    grades[id][1] += mark #sum of grades is incremented
 
-def average(grades):
-    length = len(grades)
-    sum = 0
-    for grade in grades:
-        sum += grade
-    return sum / length
+c.execute('CREATE TABLE IF NOT EXISTS stu_avg (id INTEGER PRIMARY_KEY, gpa REAL);')
 
-for id in grades.keys():
-    averages[id] = average(grades[id])
+for id in dictionary.keys():
 
-for id in names.keys():
-    print(f'name: {names[id]}', f'id: {id}', f'average: {averages[id]}', sep=" // ")
+    name = dictionary[id][0]
+    gpa = dictionary[id][2] / dictionary[id][1]
+
+    c.execute(f'INSERT INTO stu_avg (id, gpa) VALUES ({id}, {gpa});')
+    
+    print(f'name: {name}', f'id: {id}', f'average: {gpa}', sep=" // ")
+
+db.commit()
 db.close()
