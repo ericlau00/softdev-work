b918867e575c41502ab1106fed42f4d92651310d
ericlau00 <ericlau41802@gmail.com>
2019-11-13 20:08:53 -0500

    flask refactoring NTTSTT, add jinja templating, add back home link

---
+++ 25_restrio/app.py
@@ -0,0 +1,56 @@
+#Team Full-RESTore | Kevin Li & Eric Lau
+#SoftDev1 pd1
+#K25 -- Getting more REST
+#2019-11-14
+
+from flask import Flask, render_template
+import urllib.request
+import json
+
+app = Flask(__name__)
+
+def get_data(link):
+    url = urllib.request.urlopen(link)
+    response = url.read()
+    data = json.loads(response)
+    return data 
+
+@app.route("/")
+def home():
+    return render_template(
+        "index.html",
+        title = "3 REST APIs Demo")
+
+@app.route("/countries")
+def countries():
+    data = get_data("https://restcountries.eu/rest/v2/name/united%20states%20of%20america")
+
+    return render_template(
+        "countries.html", 
+        title = "Countries API Demo",
+        flag=data[0]['flag'], 
+        name=data[0]['name'])
+
+@app.route("/rick")
+def rick():
+    data = get_data("https://rickandmortyapi.com/api/character/1")
+
+    return render_template(
+        "rick.html", 
+        title = "Rick and Morty API Demo",
+        avatar=data['image'], 
+        name=data['name'])
+
+@app.route("/met")
+def met():
+    data = get_data("https://collectionapi.metmuseum.org/public/collection/v1/objects/208218")
+
+    return render_template(
+        "met.html", 
+        title = "Met Museum API Demo",
+        image=data['primaryImage'], 
+        name=data['objectName'])
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
