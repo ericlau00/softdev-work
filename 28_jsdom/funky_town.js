e5bd863a692a9a5920c66ff9ced537c02733a99b
ericlau00 <ericlau41802@gmail.com>
2019-12-11 20:52:26 -0500

    match w yaru

--- 28_jsdom/funky_town.js
+++ 28_jsdom/funky_town.js
@@ -1,53 +1,77 @@
-// Eric "Morty" Lau
-// SoftDev1 pd1
-// K28 -- Sequential Progression II: Electric Boogaloo
-// 2019-12-11
+/*
+  Eric "Morty" Lau
+  SoftDev1 pd1
+  K28 --  Sequential Progression II: Electric Boogaloo...
+  2019-12-11
+*/
 
-var fact = function (n) {
-    if (n < 2) {
+var students = ["alnasserA", "caoT", "chenA", "chenH", "chenJ", "dossE", "elahiT", "galakhovI",
+    "gorbachevY", "hallJ", "lamE", "lauE", "leeJ", "leeJo", "leeR", "liK", "linJ",
+    "linM", "luoY", "ngK", "rawatP", "shawJ", "sofatV", "vuksanajK", "walshE",
+    "xiedengD", "zhangE", "zhangM", "zhengA"]
+
+// factorial
+var fact = function (num) {
+    if (num < 2) {
         return 1;
+    } else {
+        return num * fact(num - 1);
     }
-    else {
-        return n * fact(n - 1);
-    }
-
 };
 
-var fib = function (n) {
-    if (n == 0) {
-        return 0;
-    }
-    else if (n == 1) {
+// fibonacci
+var fib = function (num) {
+    if (num > 2)
+        return fib(num - 2) + fib(num - 1);
+    else
         return 1;
-    }
-    else {
-        return fib(n - 1) + fib(n - 2);
-    }
-}
+};
 
+// greatest common denominator
 var gcd = function (a, b) {
-    var min, max;
-    if (a > b) {
-        min = b;
-        max = a;
+    if (b == 0) {
+        return a;
     }
-    else {
-        min = a;
-        max = b;
-    }
-    var counter = min;
-    while (min > 0) {
-        if (min % counter == 0 && max % counter == 0) {
-            return counter;
-        }
-        counter--;
-    }
-}
+    return gcd(b, a % b);
+};
 
-list = ["a", "b", "c", "d"]
+// random student
+var randomStudent = function () {
+    return students[Math.floor(Math.random() * students.length)];
+};
 
-var randStudent = function () {
-    var len = list.length;
-    var rand = Math.floor(Math.random() * len);
-    return (list[rand]);
-}
+// factorial button
+var factButton = document.getElementById('fact');
+var displayFact = function () {
+    console.log('fact( 9)');
+    console.log(fact(9));
+    document.getElementById('factAnswer').innerHTML = fact(9);
+};
+factButton.addEventListener('click', displayFact)
+
+// fibonacci button
+var fibButton = document.getElementById('fib');
+var displayFib = function () {
+    console.log('fib( 9)');
+    console.log(fib(9));
+    document.getElementById('fibAnswer').innerHTML = fib(9);
+};
+fibButton.addEventListener('click', displayFib)
+
+// gcd button
+var gcdButton = document.getElementById('gcd');
+var displayGCD = function () {
+    console.log('gcd( 24, 36)');
+    console.log(gcd(24, 36));
+    document.getElementById('gcdAnswer').innerHTML = gcd(24, 36);
+};
+gcdButton.addEventListener('click', displayGCD)
+
+// random student button
+var randomButton = document.getElementById('random');
+var displayRand = function () {
+    console.log('randomStudent()');
+    console.log(randomStudent());
+    document.getElementById('randAnswer').innerHTML = randomStudent();
+};
+randomButton.addEventListener('click', displayRand)
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # software-development
-Software Development w/ Mr. Brown Mykolyk at Stuyvesant 2019-2020
+
+Software Development w/ Topher Brown Mykolyk at Stuyvesant 2019-2020
 
 This repository contains most work assignments from the first semester of the course.
