def671ce79cf3706c97e3d0d8a5b09ea4ceb96c4
ericlau00 <ericlau41802@gmail.com>
2020-02-11 08:30:39 -0500

    Complete assignment

--- 06_canvas/dotconn.js
+++ 06_canvas/dotconn.js
@@ -1,7 +1,7 @@
 // Eric Lau
 // SoftDev2 pd1
 // K06 -- Dot Dot Dot...
-// 2020-02-11
+// 2020-02-12
 
 const canvas = document.getElementById('playground');
 const context = canvas.getContext('2d');
@@ -11,8 +11,8 @@ var lastX;
 var lastY;
 
 const draw = (e) => {
-    x = e.offsetX;
-    y = e.offsetY;
+    let x = e.offsetX;
+    let y = e.offsetY;
 
     // draw a line to the previous point
     context.beginPath();
@@ -39,4 +39,3 @@ const clear = () => {
 
 canvas.addEventListener('click', draw);
 clearButton.addEventListener('click', clear);
-
