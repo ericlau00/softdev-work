8e81e07f1392f459da707ed2860f6a424fae85d2
Eric Lau <ericlau41802@gmail.com>
2020-02-26 13:59:21 +0000

    Retrieve data

--- 09_mongo/app.py
+++ 09_mongo/app.py
@@ -1,2 +1,9 @@
 from pymongo import MongoClient
 
+client = MongoClient('localhost', 27017)
+db = client.test_database
+
+collection = db.test_collection
+
+file = open('primer-dataset.json', 'r').read().split('\n')
+
