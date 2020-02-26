61e08bc5c6050ac28e8c05f7f9249f6f29847b68
Eric Lau <ericlau41802@gmail.com>
2020-02-26 19:12:05 +0000

    Add json to db

--- 09_mongo/app.py
+++ 09_mongo/app.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+import json
 
 client = MongoClient('localhost', 27017)
 db = client.test_database
@@ -7,3 +8,14 @@ collection = db.test_collection
 
 file = open('primer-dataset.json', 'r').read().split('\n')
 
+items = list()
+
+for item in file:
+	items.append(json.loads(item.replace('$', '')))
+
+result = collection.insert_many(items)
+
+# print(file[0], file[1])
+print(result.inserted_ids)
+
+print(collection.count_documents({}))
