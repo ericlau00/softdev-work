65d67b2fb4bf98e27c29acb4ea697413fbdd4b7e
ericlau00 <ericlau41802@gmail.com>
2020-03-03 15:27:00 -0500

    Add block comment

--- 10_mongo/db_manager.py
+++ 10_mongo/db_manager.py
@@ -1,4 +1,4 @@
-# Amanda Zheng, Eric "Morty" Lau
+# Amanda Zheng, Eric "Morty" Lau (alez_trebek)
 # SoftDev1 pd1
 # K10 -- Import/Export Bank
 # 2020-03-04
@@ -8,21 +8,21 @@ name: 200,000+ Jeopardy! Questions JSON
 description: a JSON file containing 216,930 Jeopardy questions, answers, and other data.
 hyperlink: https://drive.google.com/file/d/0BwT5wj_P7BKXb2hfM3d2RHU1ckE/view?usp=sharing
 import mechanism:
+Manually remove a lot of the data in the JSON file because there is too much data!
+Open the file and use json.load which can read from the file to create a list of dictionaries.
+For every dictionary in the list, insert that dictionary into the database.
 '''
 
-from bson.json_util import loads
 from pymongo import MongoClient
+import json
 
 client = MongoClient("localhost", 27017)
-quiz = client.jeopardy.quiz
-
-
-def insertData(filename):
-    file = open(filename, "r")
-    doc = file.readlines()
-    for line in doc:
-        print(line)
-        # quiz.insert_one(loads(line))
+quiz = client.alez_trebek.quiz
+quiz.drop()
+file = open("alez_trebek.json", "r")
+doc = json.load(file)
+for line in doc:
+    quiz.insert_one(line)
 
 
 def findTopic(q):
@@ -50,49 +50,45 @@ def doubleJeopardy():
     return quiz.find({"round": "Double Jeopardy!"}, {"question": 1, "_id": 0})
 
 
-print("\nInserting data\n")
+print("\nPrinting category of the question: William Pereira ercted his Transmaerica \"Pyramid\" in this city\n")
 
-insertData("teamname.json")
+for result in findTopic("'William Pereira erected his Transamerica \"Pyramid\" in this city'"):
+    if(result["category"] == ""):
+        print("No Questions Found")
+    else:
+        print(result["category"].strip("'"))
 
-# print("\nPrinting category of a question\n")
-
-# for result in findTopic("'William Pereira erected his Transamerica \"Pyramid\" in this city'"):
-#     if(result["category"] == ""):
-#         print("No Questions Found")
-#     else:
-#         print(result["category"])
-
-# print("\nPrinting all questions of a category\n")
-# for result in category("HISTORY"):
-#     if (result["question"] == ""):
-#         print("No Questions Found")
-#     else:
-#         print(result["question"])
+print("\nPrinting all questions of a category\n")
+for result in category("HISTORY"):
+    if (result["question"] == ""):
+        print("No Questions Found")
+    else:
+        print(result["question"].strip("'"))
 
 
-# print("\nPrinting how much a question is worth\n")
+print("\nPrinting how much a question is worth: It can be a place to leave your puppy when you take a trip, or a carrier for him that fits under an airplane seat\n")
 
-# for result in questionWorth("'It can be a place to leave your puppy when you take a trip, or a carrier for him that fits under an airplane seat'"):
-#     if (result["value"] == ""):
-#         print("No Questions Found")
-#     else:
-#         print(result["value"])
+for result in questionWorth("'It can be a place to leave your puppy when you take a trip, or a carrier for him that fits under an airplane seat'"):
+    if (result["value"] == ""):
+        print("No Questions Found")
+    else:
+        print(result["value"].strip("'"))
 
-# print("\nPrinting results for Price\n")
+print("\nPrinting questions worth $600\n")
 
-# for result in price(600):
-#     if(result["question"] == ""):
-#         print("No Questions Found")
-#     else:
-#         print(result["question"])
+for result in price(600):
+    if(result["question"] == ""):
+        print("No Questions Found")
+    else:
+        print(result["question"].strip("'"))
 
-# print("\nPrinting answer to a Jeopardy Question\n")
-# for result in answer("Africa's lowest temperature was 11 degrees below zero in 1935 at Ifrane, just south of Fez in this country"):
-#     if(result["answer"] == ""):
-#         print("No Questions Found")
-#     else:
-#         print(result["answer"])
+print("\nPrinting answer to a Jeopardy Question: Africa's lowest temperature was 11 degrees below zero in 1935 at Ifrane, just south of Fez in this country\n")
+for result in answer("Africa's lowest temperature was 11 degrees below zero in 1935 at Ifrane, just south of Fez in this country"):
+    if(result["answer"] == ""):
+        print("No Questions Found")
+    else:
+        print(result["answer"].strip("'"))
 
-# print("\nPrinting Double Jeopardy Questions\n")
-# for result in doubleJeopardy():
-#     print(result["question"])
+print("\nPrinting Double Jeopardy Questions\n")
+for result in doubleJeopardy():
+    print(result["question"].strip("'"))
