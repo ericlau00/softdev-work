c1891bd7be7348a2f1b045a9d0eec3a62cf67d12
ericlau00 <ericlau41802@gmail.com>
2020-03-28 20:35:40 -0400

    Complete clearing function

---
+++ 12_svg-dot/README.md
@@ -0,0 +1,13 @@
+# K12: Connect the Dots
+
+Due **M 2020-03-30** 08:00 EST
+
+Estimated time required: <=60min
+
+Recreate your connect-the-dots HTML5/JS program using SVG instead of the canvas.
+
+- Each time the user clicks inside the SVG area, a circle should be drawn.
+- After the first circle, every subsequent click should also result in a line from from the previous circle to the next.
+- Include a clear button. (*Note: clearRect() will not work; it is a canvas method. You need to remove all the circles somehow…*)
+
+Save to workshop under `12_svg-dot`.
---
+++ 12_svg-dot/dotconn.js
@@ -0,0 +1,47 @@
+// Eric Lau
+// SoftDev2 pd1
+// K16 -- Connect the Dots
+// 2020-03-30
+
+var pic = document.getElementById("vimage");
+var clearButton = document.getElementById("clearButton");
+var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+var firstClick = true;
+var oldX = null;
+var oldY = null;
+
+pic.addEventListener("click", function (e) {
+    c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+
+    c.setAttribute("cx", e.offsetX);
+    c.setAttribute("cy", e.offsetY);
+    c.setAttribute("r", "5");
+    c.setAttribute("fill", "black");
+    c.setAttribute("stroke", "black");
+
+    pic.appendChild(c);
+
+    if (firstClick) {
+        firstClick = false;
+    } else {
+        var l = document.createElementNS("http://www.w3.org/2000/svg", "line");
+
+        l.setAttribute("x1", oldX);
+        l.setAttribute("y1", oldY);
+        l.setAttribute("x2", e.offsetX);
+        l.setAttribute("y2", e.offsetY);
+        l.setAttribute("style", "stroke:black; stroke-width:1");
+
+        pic.appendChild(l);
+    }
+
+    oldX = e.offsetX;
+    oldY = e.offsetY;
+});
+
+clearButton.addEventListener("click", function (e) {
+    pic.innerHTML = "";
+    oldX = null;
+    oldY = null;
+    firstClick = true;
+});
