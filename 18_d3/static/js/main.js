eb9397fc03a6da4cbc359a2e02fa9927566c9893
ericlau00 <ericlau41802@gmail.com>
2020-04-14 17:08:18 -0400

    Add landing screen

--- 18_d3/static/js/main.js
+++ 18_d3/static/js/main.js
@@ -3,23 +3,37 @@
 // K18 -- Come Up For Air
 // 2020-04-21
 
-console.log('Hello, World!');
+let created = false;
 
-const width = 975;
-const height = 610;
-const svg = d3.select('body').append('svg').attr("viewBox", [0, 0, width, height]);
+document.getElementById('render-button').addEventListener('click', () => {
+    if (!created) {
+        let svg = createSVG();
+        render(svg);
+        created = true;
+    }
+    window.scrollTo(0, document.getElementById('svg-container').offsetTop);
+})
+
+const createSVG = () => {
+    document.getElementById('visualization').innerHTML += `
+        <div class="container-fluid vh-100">
+            <div class="row vh-100 justify-content-center align-items-center" id="svg-container">
+            </div>
+        </div>`
+    return d3.select('#svg-container').append('svg')
+        .attr("viewBox", [0, 0, 975, 610])
+        .attr("width", "50%");
+}
 
 const path = d3.geoPath();
 
-let render = async () => {
+const render = async (svg) => {
     let us = await d3.json('static/json/states-albers-10m.json');
 
     svg.append("g")
-    .selectAll("path")
-    .data(topojson.feature(us, us.objects.states).features)
-    .join("path")
-    .attr("fill", 'red')
-    .attr("d", path)
+        .selectAll("path")
+        .data(topojson.feature(us, us.objects.states).features)
+        .join("path")
+        .attr("fill", 'red')
+        .attr("d", path);
 }
-
-render();
