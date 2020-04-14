1942bf8408fdc4bab1fec9e0b454f435536ac619
ericlau00 <ericlau41802@gmail.com>
2020-04-14 00:14:30 -0400

    Add US Map

--- 18_d3/static/js/main.js
+++ 18_d3/static/js/main.js
@@ -4,3 +4,22 @@
 // 2020-04-21
 
 console.log('Hello, World!');
+
+const width = 975;
+const height = 610;
+const svg = d3.select('body').append('svg').attr("viewBox", [0, 0, width, height]);
+
+const path = d3.geoPath();
+
+let render = async () => {
+    let us = await d3.json('static/json/states-albers-10m.json');
+
+    svg.append("g")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.states).features)
+    .join("path")
+    .attr("fill", 'red')
+    .attr("d", path)
+}
+
+render();
